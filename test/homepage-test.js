5ca88ae85b4ab61213510e7780183e714c7d7c2b
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-03-26 11:37:03 +0100

    update to test file

--- test/homepage-test.js
+++ test/homepage-test.js
@@ -2,7 +2,6 @@ var expect = require('chai').expect;
 var request = require('request');
 
 describe('Homepage feature test', function () {
-
   it('Homepage content test', function (done) {
     request('http://localhost:8080' , function (error, response, body) {
       expect(body).to.equal('This is a test page');
